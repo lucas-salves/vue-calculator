499aca204fca218136983d9cb542028358e02aad
lucas.salves@terra.com.br <lucas.salves@terra.com.br>
2020-03-04 20:53:24 -0300

    Initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="calculator">
+        <div class="display">
+         
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .calculator{
+        display: grid; 
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(50px, auto);
+    }
+
+    .display{
+        grid-column: 1 / 5;
+        background-color: red;
+
+    }
+</style>
