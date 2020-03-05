611289cac42f655164368b706f4b9254add28103
lucas.salves@terra.com.br <lucas.salves@terra.com.br>
2020-03-04 22:17:06 -0300

    sign bug fix

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -39,8 +39,10 @@ export default {
             this.current = '';
         },
         sign(){
-            this.current = this.current.charAt(0) ===  '-'?
+            if (this.current != '') {                
+                this.current = this.current.charAt(0) ===  '-'?
                 this.current.slice(1) : `-${this.current}`;
+            }
         },
         percent(){
             this.current = `${parseFloat(this.current) / 100}`;  
